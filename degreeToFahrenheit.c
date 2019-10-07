b1295c7f67b92ef351d32753e7b0f0226033fb79
ashutosh2513 <sharma.ashutosh133@gmail.com>
2019-10-07 18:46:36 +0530

    Funny Program

---
+++ degreeToFahrenheit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+printf("\t\t\taa gyea pher convert krn\n\n");
+printf("\t\t\tchll pehla degree toh fahrenheit vch krde aa\n\n");
+
+float c;
+float g;
+printf("\n\thon ds de koi number jeada mein convert kra:");
+scanf("%f",&c);
+g=((9*c)/5)+32;
+printf("\n\n\t\taa le chk fahrenheit vch:%.3f\n\n\n\n\n",g);
+
+
+
+
+}
+
