ee1eeb32bf7ccf3b2ce0b1ab0adb5dc10a30b218
ashutosh2513 <sharma.ashutosh133@gmail.com>
2019-10-09 10:19:23 +0530

    Use of Switch

--- MyOwnCalculator.c
+++ MyOwnCalculator.c
@@ -3,6 +3,16 @@ int main()
 {
 char ca;
 float a,b;
+printf(" _________________\n ");
+printf("|_______________|\n");
+printf(" | 1 | 2 | 3 |   |\n");
+printf(" |___|___|___|   |\n");
+printf(" | 4 | 5 | 6 | + |\n");
+printf(" |___|___|___|___|\n");
+printf(" | 7 | 8 | 9 | - |\n");
+printf(" |___|___|___|___|\n");
+printf(" |    0      | * |\n");
+printf(" |___________|___|\n");
 
 printf("\n\n\t\tWhat do u want to do: +, -, /, * :");
 scanf("%c",&ca);
@@ -13,13 +23,43 @@ scanf("%f%f",&a,&b);
 switch(ca)
 {
 case '+':
-printf("%.1f + %.1f = %.1f\n",a,b,a+b);
+printf(" _________________\n ");
+printf("|%.1f+%.1f = %.1f\n",a,b,a+b);
+printf(" |_______________|\n");
+printf(" | 1 | 2 | 3 |   |\n");
+printf(" |___|___|___|   |\n");
+printf(" | 4 | 5 | 6 | + |\n");
+printf(" |___|___|___|___|\n");
+printf(" | 7 | 8 | 9 | - |\n");
+printf(" |___|___|___|___|\n");
+printf(" |    0      | * |\n");
+printf(" |___________|___|\n");
 break;
 case '-':
-printf("%.1f - %.1f = %.1f\n",a,b,a-b);
+printf(" _________________\n ");
+printf("|%.1f-%.1f = %.1f\n",a,b,a-b);
+printf(" |_______________|\n");
+printf(" | 1 | 2 | 3 |   |\n");
+printf(" |___|___|___|   |\n");
+printf(" | 4 | 5 | 6 | + |\n");
+printf(" |___|___|___|___|\n");
+printf(" | 7 | 8 | 9 | - |\n");
+printf(" |___|___|___|___|\n");
+printf(" |    0      | * |\n");
+printf(" |___________|___|\n");
 break;
 case '*':
-printf("%.1f * %.1f = %.1f\n",a,b,a*b);
+printf(" _________________\n ");
+printf("|%.1f*%.1f = %.1f\n",a,b,a*b);
+printf(" |_______________|\n");
+printf(" | 1 | 2 | 3 |   |\n");
+printf(" |___|___|___|   |\n");
+printf(" | 4 | 5 | 6 | + |\n");
+printf(" |___|___|___|___|\n");
+printf(" | 7 | 8 | 9 | - |\n");
+printf(" |___|___|___|___|\n");
+printf(" |    0      | * |\n");
+printf(" |___________|___|\n");
 break;
 case '/':
 if(b==0)
@@ -28,7 +68,17 @@ printf("Not possibe");
 }
 else
 {
-printf("%.1f / %.1f = %.2f\n",a,b,a/b);
+printf(" _________________\n ");
+printf("|%.1f/%.1f = %.2f\n",a,b,a/b);
+printf(" |_______________|\n");
+printf(" | 1 | 2 | 3 |   |\n");
+printf(" |___|___|___|   |\n");
+printf(" | 4 | 5 | 6 | + |\n");
+printf(" |___|___|___|___|\n");
+printf(" | 7 | 8 | 9 | - |\n");
+printf(" |___|___|___|___|\n");
+printf(" |    0      | * |\n");
+printf(" |___________|___|\n");
 }
 break;
 default:
