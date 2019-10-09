97f3d3224cd014a6625021b60d7e707999badc09
ashutosh2513 <sharma.ashutosh133@gmail.com>
2019-10-09 10:56:08 +0530

    Int, float, char, string

---
+++ inputany.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a;
+float b;
+char g;
+printf("Enter any interger:\n");
+scanf("%d",&a);
+printf("Enter any decimal number:\n");
+scanf("%f",&b);
+printf("Enter any character:\n");
+scanf(" %c",&g);
+char name[220];
+printf("Enter any name:\n");
+scanf("%s",&name);
+}
+
