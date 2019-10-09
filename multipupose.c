df655525b42dca256572095a6c2975b9b2d8733b
ashutosh2513 <sharma.ashutosh133@gmail.com>
2019-10-09 10:51:58 +0530

    add, multy, sub., div.

---
+++ multipupose.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,f,g;
+printf("Enter two numbers:");
+scanf("%d %d",&a,&b);
+c=a+b;
+d=a*b;
+e=a/b;
+f=a-b;
+g=a%b;
+printf("On adding these two number:%d\n",c);
+printf("On subtracting these two number:%d\n",f);
+printf("On multipilation these two number:%d\n",d);
+printf("On division these two number:%d\n",e);
+printf("Mod of these two number:%d\n",g);
+}
+
